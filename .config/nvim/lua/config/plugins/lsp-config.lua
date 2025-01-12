faf333629d290c528e5e2d3783569fde1efaa352
stefin <stefinracho@gmail.com>
2025-01-11 21:58:17 -0800

    fix: nvim

---
+++ .config/nvim/lua/config/plugins/lsp-config.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    opts = {
+      servers = {
+        lua_ls = {},
+        clangd = { cmd = { "clangd", "--fallback-style=webkit" } },
+        pyright = {}
+      }
+    },
+    config = function(_, opts)
+      local lspconfig = require('lspconfig')
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client then
+            return
+          end
+
+          if client.supports_method("textDocument/formatting") then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
+}
