366b15fa79e5ce77e4b4e405c94830804f27e575
stefin <stefinracho@gmail.com>
2025-05-31 17:58:40 -0700

    manual format

--- .config/nvim/lua/config/plugins/lsp-config.lua
+++ .config/nvim/lua/config/plugins/lsp-config.lua
@@ -38,17 +38,10 @@ return {
           if not client then
             return
           end
-
-          if client.supports_method("textDocument/formatting") then
-            -- Format the current buffer on save
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end,
-            })
-          end
         end,
+
+        vim.keymap.set('n', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>');
+        vim.keymap.set('v', '<leader>gf', '<cmd>lua vim.lsp.buf.format({async = true})<CR>');
       })
     end,
   },
