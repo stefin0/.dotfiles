b536ba66ec40b6e7ab3396e3e398f28ae689138f
stefin <stefinracho@gmail.com>
2025-10-18 06:58:22 -0700

    set prettierd as css formatter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
 	opts = {
 		formatters_by_ft = {
 			c = { "clang-format" },
+			css = { "prettierd" },
 			html = { "prettierd" },
 			javascript = { "prettierd" },
 			json = { "prettier" },
